6a36f20877f44c343e3732b1d1af15792219810a
emon22830 <mdemon22830@gmail.com>
2024-08-14 11:01:47 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'pages/counter_pages.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterPage(),
+    );
+  }
+}
